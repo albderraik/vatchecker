8fb00e960fcdde07881f36e21994d13e3a69c36e
Andre B. Derraik <andre@tecgraf.puc-rio.br>
2017-09-20 18:28:21 -0300

    Adjusting the script.

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
 <?php
 DEFINE ('VIES_URL', 'http://ec.europa.eu/taxation_customs/vies/services/checkVatService');
+DEFINE ('DEBUG', false);
 
-// Check parameters
-$message = "";
-$country = isset( $_POST['country'] ) ? $_POST['country'] : '';
-$number = isset( $_POST['number'] ) ? $_POST['number'] : '';
-function checkParameters() {
-	if (! isset($_POST["country"])) {
-		$message = 'Error: Need two letters country code.';
-		return false;
-	}
-	elseif (! isset($_POST["number"])) {
-		$message = 'Error: Need VAT number.';
-		return false;
-	}
-
-	// Get values
-	$country = $_POST["country"];
-	$number = $_POST["number"];
-	if (strlen($country) != 2) {
-		$message = 'Error: invalid coutry code. Need to be two letters country code.';
-		return false;
-	}
-	return true;
-}
+// Initialize variables
+$message = '';
+$country = '';
+$number = '';
+$vat = isset($_GET['vat']) ? $_GET['vat'] : '';
 
 /**
  * VIES VAT number validation
@@ -36,8 +19,7 @@ function checkParameters() {
  */
 function viesCheckVAT($countryCode, $vatNumber, $timeout = 30) {
 	$response = array ();
-	$pattern = "/<(%s).*?>([\s\S]*)<\/\1/";
-	$pattern_error = "/<soap:(%s).*?>([\s\S]*)<\/\1/";
+	$pattern = '/<(%s).*?>([\s\S]*)<\/\1/';
 	$keys = array (
 		'countryCode',
 		'vatNumber',
@@ -74,34 +56,59 @@ function viesCheckVAT($countryCode, $vatNumber, $timeout = 30) {
 	$ctx = stream_context_create($opts);
 	$result = file_get_contents(VIES_URL, false, $ctx);
 
-//	print_array($result);
+	print_array($result);
 
 	// Process return
-	if (preg_match(sprintf($pattern, 'checkVatResponse'), $result, $matches)) {
-		print_array('OK');
+	$check = preg_match(sprintf($pattern, 'checkVatResponse'), $result, $matches);
+	if (DEBUG) print('<br>Check 1: '.$check);
+	if ($check == 1) {
+		print_array($matches);
 		foreach($keys as $key)
 			preg_match(sprintf($pattern, $key), $matches[2], $value) && $response[$key] = $value[2];
-	} else if (preg_match(sprintf($pattern_error, 'soap:Fault'), $result, $matches)) {
-		print_array('ERROR');
-		foreach($keys as $key)
-			preg_match(sprintf($pattern, $key), $matches[2], $value) && $response[$key] = $value[2];
+	} else {
+		$check = preg_match(sprintf($pattern, 'soap:Fault'), $result, $matches);
+		if (DEBUG) print('<br>Check 2: '.$check);
+		if ($check == 1) {
+			print_array($matches);
+			foreach($keys as $key)
+				preg_match(sprintf($pattern, $key), $matches[2], $value) && $response[$key] = $value[2];
+		}
 	}
 	return $response;
 }
 
 function print_array($aArray) {
+	if (!DEBUG) return;
     // Print a nicely formatted array representation:
 	echo '<pre>';
 	print_r($aArray);
 	echo '</pre>';
 }
 
-// Do the job
-if (checkParameters()) {
-	$result = viesCheckVAT(strtoupper($country), $number);
-	$message = isset( $result['faultstring'] ) ? $result['faultstring'] : '';
-	if ($message == '') {
-		$message = "Valid VAT code.";
+// Check parameter
+if ($vat == '') {
+	$message = 'Error: Invalid VAT code.';
+} else {
+	// Get values
+	$country = substr($vat, 0, 2);
+	$number = substr($vat, 2);
+	if ($country == '') {
+		$message = 'Error: Need two letters country code. [' . $country . ']';
+	} else if ($number == '') {
+		$message = 'Error: Invalid number code. [' . $number . ']';
+	} else {
+		// Do the job
+		if (DEBUG) print('Country: [' . $country . ']<br>');
+		if (DEBUG) print('Number: [' . $number . ']<br>');
+		$result = viesCheckVAT(strtoupper($country), $number);
+		// Check results
+		if (isset($result['valid'])) {
+			$message = ($result['valid'] == 'true' ? "Valid VAT code." : "Invalid VAT code.");
+		} else {
+			$message = isset($result['faultstring']) ? $result['faultstring'] : '';
+			if ($message == '')
+				$message = "Valid VAT code.";
+		}
 	}
 }
 ?>
@@ -112,14 +119,6 @@ if (checkParameters()) {
 </head>
 <body>
 	<h3> <?php print $message ?> </h3>
-	<h3> <?php if (isset($country) && $country != '') print 'Country: ' . $country ?> </h3>
-	<h3> <?php if (isset($number) && $number != '') print 'Number: ' . $number ?> </h3>
-	<form method="POST">
-	Type VAT to check:<br />
-	<label>Country:</label><input type="text" name="country" value="<?php echo $country ?>"><br />
-	<label>Number:</label><input type="text" name="number" value="<?php echo $number ?>"><br />
-	<input type="submit" value="submit">
-	</form>
 </body>
 </html>
 
